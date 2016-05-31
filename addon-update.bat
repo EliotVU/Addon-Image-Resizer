04361ec0d2c5018b9328a3797658cdfe67fc77a7
Eliot van Uytfanghe <eliot_van@hotmail.com>
2016-05-31 07:50:37 +0200

    Move from CFX to JPM.

---
+++ addon-update.bat
@@ -0,0 +1,5 @@
+:reload
+call addon-xpi
+wget --post-file=jid0-hd39BGK3EuIbK47rGW3fZdR163o@jetpack-1.6.0.xpi http://localhost:8888/
+pause
+goto reload
---
+++ addon-watch.bat
@@ -0,0 +1,3 @@
+cd dist
+jpm watchpost --post-url http://localhost:8888/
+pause
